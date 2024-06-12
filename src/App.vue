5ece1951ab83953fba9b078b56515df6074f3b79
azizkhonteacher <azizkhonteacher@gmail.com>
2024-06-12 01:28:24 -0700

    update

---
+++ src/App.vue
@@ -0,0 +1,93 @@
+<template>
+  <div>
+    <header>
+      <div class="header-sub">
+        <div class="container">
+          <button class="header-sub__lang">
+            <p class="header-sub__lang-p">Узбекча</p>
+            <svg
+              class="header-sub__lang-down"
+              viewBox="0 0 14 14"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M3.0876 5.42085C3.31541 5.19305 3.68475 5.19305 3.91256 5.42085L7.00008 8.50837L10.0876 5.42085C10.3154 5.19305 10.6848 5.19305 10.9126 5.42085C11.1404 5.64866 11.1404 6.01801 10.9126 6.24581L7.41256 9.74581C7.18476 9.97362 6.81541 9.97362 6.5876 9.74581L3.0876 6.24581C2.8598 6.01801 2.8598 5.64866 3.0876 5.42085Z"
+                fill="white"
+              />
+            </svg>
+          </button>
+
+          <h4 class="header-sub__link">
+            <a href="https://t.me/talaba616">«Фарғоналикларни»</a> телеграмда
+            кузатинг!
+          </h4>
+
+          <svg
+            class="header-sub__search"
+            viewBox="0 0 22 22"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M9.16659 3.66667C6.12902 3.66667 3.66659 6.1291 3.66659 9.16667C3.66659 12.2042 6.12902 14.6667 9.16659 14.6667C12.2042 14.6667 14.6666 12.2042 14.6666 9.16667C14.6666 6.1291 12.2042 3.66667 9.16659 3.66667ZM1.83325 9.16667C1.83325 5.11658 5.1165 1.83333 9.16659 1.83333C13.2167 1.83333 16.4999 5.11658 16.4999 9.16667C16.4999 10.8613 15.9251 12.4217 14.9598 13.6635L19.8981 18.6018C20.2561 18.9598 20.2561 19.5402 19.8981 19.8982C19.5401 20.2562 18.9597 20.2562 18.6017 19.8982L13.6634 14.9599C12.4216 15.9252 10.8612 16.5 9.16659 16.5C5.1165 16.5 1.83325 13.2168 1.83325 9.16667Z"
+              fill="#727272"
+            />
+          </svg>
+        </div>
+      </div>
+
+      <div class="header-navbar">
+        <div class="container">
+          <a href="#" class="header-navbar__logo">
+            <img src="./assets/images/site-logo.png" alt="img" />
+          </a>
+
+          <ul class="header-navbar__nav">
+            <li class="header-navbar__li"><a href="#">Ўзбекистон</a></li>
+            <li class="header-navbar__li"><a href="#">Технология</a></li>
+            <li class="header-navbar__li"><a href="#">Жахон</a></li>
+            <li class="header-navbar__li"><a href="#">Фарғона</a></li>
+            <li class="header-navbar__li"><a href="#">Жамият</a></li>
+            <li class="header-navbar__li"><a href="#">Сиёсат</a></li>
+            <li class="header-navbar__li"><a href="#">Муҳокама</a></li>
+            <li class="header-navbar__li"><a href="#">Спорт</a></li>
+          </ul>
+        </div>
+      </div>
+    </header>
+
+    <footer>
+      <div class="footer-logo">
+        <img
+          src="./assets/images/site-logo.png"
+          alt="img"
+          class="footer-logo__img"
+        />
+      </div>
+
+      <div class="footer-nav">
+        <ul class="footer-nav__navbar">
+          <li class="footer-nav__li"><a href="#">Ўзбекистон</a></li>
+          <li class="footer-nav__li"><a href="#">Технология</a></li>
+          <li class="footer-nav__li"><a href="#">Жахон</a></li>
+          <li class="footer-nav__li"><a href="#">Фарғона</a></li>
+          <li class="footer-nav__li"><a href="#">Жамият</a></li>
+          <li class="footer-nav__li"><a href="#">Сиёсат</a></li>
+          <li class="footer-nav__li"><a href="#">Муҳокама</a></li>
+          <li class="footer-nav__li"><a href="#">Спорт</a></li>
+        </ul>
+
+        <p class="footer-nav__ptitle">
+          2021 © Фарғоналиклар Барча ҳуқуқлар ҳимоя қилинган
+        </p>
+      </div>
+    </footer>
+
+    <footer></footer>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
